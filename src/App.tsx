1ac3764c5e1098f53455e96d1fad6b22a9b827f4
Erika <erroblesr@gmail.com>
2025-12-05 16:05:44 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,42 @@ function App() {
   const {pokemon, loading,error, fetchPokemon} = usePokemon();
   const [name,setName] = useState("");
   return (
-    <>
-      <input 
-        value={name}
-        type="text" 
-        onChange={(e)=>setName(e.target.value)} 
-        placeholder='Search Pokemon'/>
-      <button onClick={()=>fetchPokemon(name)}> Search</button>
-      {loading && <p>Loading...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {pokemon && (
-        <div>
-          <h2>Name:{pokemon.name}</h2>
-          <img src={pokemon.sprites.front_default} alt="image of pokemon" />
-          <p>Height:{pokemon.height}</p>
-          <p>Weight:{pokemon.weight}</p>
-          <h3>Types:</h3>
-          <ul>
-            {pokemon.types.map((t, i) => (
-              <li key={i}>{t.type.name}</li>
-            ))}
-          </ul>
-          <h3>Abilities:</h3>
-          <ul>
-            {pokemon.abilities.map((a,i)=>(
-              <li key={i}>{a.ability.name}</li>
-            ))}
-          </ul>
+    <div className='poke-background'> 
+        <div className='app-container'>
+        <div className='search-container'>
+          <input 
+          className='search-bar'
+          value={name}
+          type="text" 
+          onChange={(e)=>setName(e.target.value)} 
+          placeholder='Search Pokemon'/>
+          <button className='search-btn' onClick={()=>fetchPokemon(name)}> Search</button>
         </div>
-      )}
-    </>
+        
+        {loading && <p>Loading...</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {pokemon && (
+          <div className='pokemon-card'>
+            <h2>Name: {pokemon.name}</h2>
+            <img src={pokemon.sprites.front_default} alt="image of pokemon" />
+            <p>Height: {pokemon.height}</p>
+            <p>Weight: {pokemon.weight}</p>
+            <h3>Types:</h3>
+            <ul>
+              {pokemon.types.map((t, i) => (
+                <li key={i}>{t.type.name}</li>
+              ))}
+            </ul>
+            <h3>Abilities:</h3>
+            <ul>
+              {pokemon.abilities.map((a,i)=>(
+                <li key={i}>{a.ability.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
